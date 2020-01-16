2f4b0eec4a989667f2ad0d341ed82d52106ec5ed
makeng <mazhaokeng@orvibo.com>
2020-01-16 10:02:04 +0800

    🌟：【全局】优化「状态模式」。

--- Process-状态(🍬️).js
+++ Process-状态(🍬️).js
@@ -67,7 +67,7 @@ class BadCandyMachine {
 
 // 测试：能用，但是一旦增加一个状态，就要修改所有的代码
 const badCandyMachine = new BadCandyMachine(1)
-log('/*------ BadCandyMachine ------*/')
+log('/*------ Bad CandyMachine ------*/')
 log('/*-- 1 --*/')
 badCandyMachine.insertCoin()
 badCandyMachine.pullTrigger()
@@ -76,6 +76,91 @@ badCandyMachine.pullTrigger()
 log('/*-- 3 --*/')
 badCandyMachine.insertCoin()
 badCandyMachine.pullTrigger()
+
+
+// 重新设计：封装状态，而不是封装转换方法
+class CandyMachine {
+  constructor (candyCnt) {
+    this.candyCnt = candyCnt || 0
+    this.state = STATE_NO_QUARTER
+  }
+
+  insertCoin () {
+    log('You inserted a quarter.')
+    if (this.state === STATE_HAS_QUARTER) {
+      log('Already has a quarter.')
+    }
+    this._setState(STATE_HAS_QUARTER)
+  }
+
+  pullTrigger () {
+    log('You pulled the trigger.')
+    if (this.state === STATE_HAS_QUARTER) {
+      this._setState(STATE_SOLD)
+    } else {
+      log('No quarter.')
+    }
+  }
+
+  refill () {
+    log('Candy refill.')
+    this.candyCnt += 1
+    this._setState(STATE_NO_QUARTER)
+  }
+
+  _setState (state) {
+    this.state = state
+    const fn = {
+      STATE_NO_QUARTER: () => this._noQuarter(),
+      STATE_HAS_QUARTER: () => this._hasQuarter(),
+      STATE_SOLD: () => this._sold(),
+      STATE_SOLD_OUT: () => this._soldOut()
+    }[state]
+    fn()
+  }
+
+  dispense () {
+    if (this.candyCnt > 0) {
+      this.candyCnt -= 1
+      log('Dispense candy.')
+      this._setState(STATE_NO_QUARTER)
+    } else {
+      this._setState(STATE_SOLD_OUT)
+    }
+  }
+
+  _noQuarter () {
+    log('No quarter.')
+  }
+
+  _hasQuarter () {
+    log('There is quarter.')
+  }
+
+  _sold () {
+    this.dispense()
+  }
+
+  _soldOut () {
+    log('Candy sold out.')
+  }
+}
+
+const candyMachine = new CandyMachine(1)
+log('/*------ Good CandyMachine ------*/')
+log('/*-- 1 --*/')
+candyMachine.insertCoin()
+candyMachine.pullTrigger()
+log('/*-- 2 --*/')
+candyMachine.pullTrigger()
+log('/*-- 3 --*/')
+candyMachine.insertCoin()
+candyMachine.pullTrigger()
+log('/*-- 4 --*/')
+candyMachine.insertCoin()
+candyMachine.refill()
+candyMachine.pullTrigger()
+
 
 
 
