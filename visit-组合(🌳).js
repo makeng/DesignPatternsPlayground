01b35bf42c38b419fd46162e63a3440d3c1d70db
makeng <810768333@qq.com>
2020-11-05 11:30:43 +0800

    🌟：【JS】增加注释

--- visit-组合(🌳).js
+++ visit-组合(🌳).js
@@ -1,18 +1,20 @@
 /* ---------------------------------------------------------------------------------------
-* about:组合模式
+* about:组合模式是一种结构型设计模式， 你可以使用它将对象组合成树状结构， 并且能像使用独立对象一样使用它们。
+* 该方式的最大优点在于你无需了解构成树状结构的对象的具体类。 你也无需了解对象是简单的产品还是复杂的盒子。
+* 你只需调用通用接口以相同的方式对其进行处理即可。 当你调用该方法后， 对象会将请求沿着树结构传递下去。
 * author:马兆铿（13790371603 810768333@qq.com）
 * date:2019-01-13
 * ---------------------------------------------------------------------------------------- */
 class PancakeHouseMenu {
-  constructor () {
+  constructor() {
     this.list = [] // Array
   }
 
-  addItem (name, price) {
+  addItem(name, price) {
     this.list.push(new MenuItem(name, price))
   }
 
-  addSubMenu (subMenuName, { name, price }) {
+  addSubMenu(subMenuName, {name, price}) {
     const subMenu = this.list.find(item => item.name === subMenuName)
     if (subMenu) {
       subMenu.addItem(name, price)
@@ -25,12 +27,12 @@ class PancakeHouseMenu {
     }
   }
 
-  getMenuList () {
+  getMenuList() {
     return this.list
   }
 
   // 迭代器
-  each (fn) {
+  each(fn) {
     const list = this.getMenuList()
     // 分类型处理
     list.forEach((item, index) => {
@@ -43,7 +45,7 @@ class PancakeHouseMenu {
 
 // 子菜单
 class SubMenu extends PancakeHouseMenu {
-  constructor (name) {
+  constructor(name) {
     super()
     Object.assign(this, {
       name,
@@ -53,22 +55,22 @@ class SubMenu extends PancakeHouseMenu {
 }
 
 class DinnerHouseMenu {
-  constructor () {
+  constructor() {
     this.list = {} // ArrayLike
     this.list.length = 0
   }
 
-  addItem (name, price) {
+  addItem(name, price) {
     this.list[this.list.length] = new MenuItem(name, price)
     this.list.length += 1
   }
 
-  getMenuList () {
+  getMenuList() {
     return this.list
   }
 
   // 迭代器
-  each (fn) {
+  each(fn) {
     const list = this.getMenuList()
     Array.prototype.forEach.call(list, (item, index) => fn(item, index))
   }
@@ -76,32 +78,32 @@ class DinnerHouseMenu {
 
 /* ----------------------------------------- 共用类 ----------------------------------------- */
 class MenuItem {
-  constructor (name, price) {
-    Object.assign(this, { name, price })
+  constructor(name, price) {
+    Object.assign(this, {name, price})
   }
 
-  getName () {
+  getName() {
     return this.name
   }
 
-  getPrice () {
+  getPrice() {
     return this.price
   }
 }
 
 class Maid {
-  constructor (name) {
+  constructor(name) {
     this.name = name
   }
 
   // 需要操作「每一项」
-  printMenu () {
+  printMenu() {
     Array.prototype.forEach.call(arguments, list => {
       this.printList(list)
     })
   }
 
-  printList (list) {
+  printList(list) {
     // 调用其迭代器
     list.each(item => {
       console.log(`${item.getName()}: $${item.getPrice()}`)
@@ -115,9 +117,9 @@ const dinnerHouseMenu = new DinnerHouseMenu()
 
 pancakeHouseMenu.addItem('pizza', 12)
 pancakeHouseMenu.addItem('waffle', 10)
-pancakeHouseMenu.addSubMenu('breakfast', { name: 'soup', price: 2 })
-pancakeHouseMenu.addSubMenu('breakfast', { name: 'soy milk', price: 1 })
-pancakeHouseMenu.addSubMenu('breakfast', { name: 'bread', price: 2 })
+pancakeHouseMenu.addSubMenu('breakfast', {name: 'soup', price: 2})
+pancakeHouseMenu.addSubMenu('breakfast', {name: 'soy milk', price: 1})
+pancakeHouseMenu.addSubMenu('breakfast', {name: 'bread', price: 2})
 dinnerHouseMenu.addItem('chicken', 50)
 dinnerHouseMenu.addItem('potato', 20)
 
