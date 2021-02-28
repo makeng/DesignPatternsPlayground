866ed7f5e2710eb9bfa6abcd4c09eedb20d2613f
makeng <810768333@qq.com>
2021-02-28 22:35:32 +0800

    🌟：【TS】增加说明

--- src-ts/0_Visit-Factory/index.ts
+++ src-ts/0_Visit-Factory/index.ts
@@ -1,9 +1,18 @@
+/*----------------------------------------------------------------------------------
+* TS 做了什么：
+* - 为类 NYStore 和 ChicagoStore 提供了共同的 interface
+* - Piazza 类不需要 interface，因为它没有相似的类。要弄清楚 interface 的本质。
+* - interface 保存在声明文件中，与逻辑分离
+* - 使用字面量类型，控制原料的值
+* ----------------------------------------------------------------------------------*/
 import * as types from './types'
+
+type material = 'cheese' | 'onion' | 'durian';
 
 class Pizza {
   name: string
 
-  constructor (material: string) {
+  constructor (material: material) {
     this.name = `${material} pizza`
   }
 
@@ -48,10 +57,11 @@ class ChicagoStore implements types.Store {
   }
 }
 
+/* ----------------------------------------- execution ----------------------------------------- */
 const nyStore = new NYStore('01 street')
 const chicagoStore = new ChicagoStore('53 street')
-const pizza1 = new Pizza('cheese')
-const pizza2 = new Pizza('tomato')
+const pizza1 = new Pizza('durian')
+const pizza2 = new Pizza('cheese')
 
 nyStore.bake(pizza1)
 nyStore.deliver(pizza1)
